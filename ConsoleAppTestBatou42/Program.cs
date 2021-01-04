1f09c72b445a304129c038840951ebcbc570a75a
Victor Jung <victor.jung@isen.yncrea.fr>
2021-01-04 15:39:21 +0100

    Revert "Clean of Test Files"
    This reverts commit 0ba31fb9e85bf952cece7d18d497211cdf1a3a1f.

---
+++ ConsoleAppTestBatou42/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppTestBatou42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
