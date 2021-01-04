f6c9216b9ad0e3bd4324a486d37578916d63f52b
Milawy <william.pecqueux@isen.yncrea.fr>
2021-01-04 16:25:41 +0100

    Test

---
+++ ConsoleAppTestWil/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppTestWil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
