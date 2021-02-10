d32edd0df07ddc0fbd483f14cbd027a23a5d710c
Victor Jung <victor.jung@isen.yncrea.fr>
2021-02-10 11:26:34 +0100

    Hover on boat added

--- Battleship/MainApp/MainPage.xaml.cs
+++ Battleship/MainApp/MainPage.xaml.cs
@@ -33,6 +33,7 @@ namespace MainApp
         {
             this.InitializeComponent();
             this.InitializeSea();
+            
             this.InitializeBoats();
         }
 
@@ -120,6 +121,8 @@ namespace MainApp
                 rect.Name = (name + ":" + i); // C = Carrier + rectIdx
                 rect.Fill = new SolidColorBrush(Colors.White);
                 rect.Stroke = new SolidColorBrush(Colors.DarkGray);
+                rect.PointerEntered += new PointerEventHandler(Boat_PointerEntered);
+                rect.PointerPressed += new PointerEventHandler(Boat_PointerPressed);
                 Grid.SetColumn(rect, i);
                 boatGrid.Children.Add(rect);
             }
@@ -131,6 +134,8 @@ namespace MainApp
                 rect.Name = (name + ":" + i); // C = Carrier + rectIdx
                 rect.Fill = new SolidColorBrush(Colors.White);
                 rect.Stroke = new SolidColorBrush(Colors.DarkGray);
+                rect.PointerEntered += new PointerEventHandler(Boat_PointerEntered);
+                rect.PointerPressed += new PointerEventHandler(Boat_PointerPressed);
                 Grid.SetRow(rect, i);
                 boatGrid.Children.Add(rect);
             }
@@ -151,6 +156,25 @@ namespace MainApp
             textBox1_X.Text = x.ToString();
             textBox1_Y.Text = y.ToString();
 
+            rect.Fill = new SolidColorBrush(Colors.Red);
+        }
+
+        public void Boat_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            Rectangle rect = (Rectangle)sender;
+            rect.PointerExited += new PointerEventHandler(Boat_PointerExited);
+            rect.Fill = new SolidColorBrush(Colors.Red);
+        }
+        public void Boat_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            Rectangle rect = (Rectangle)sender;
+            rect.PointerExited -= new PointerEventHandler(Boat_PointerExited);
+            rect.Fill = new SolidColorBrush(Colors.White);
+        }
+        public void Boat_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            Rectangle rect = (Rectangle)sender;
+            rect.PointerExited -= new PointerEventHandler(Boat_PointerExited);
             rect.Fill = new SolidColorBrush(Colors.Red);
         }
 
